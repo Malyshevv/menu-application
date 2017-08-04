9699ef1ee779ee6c3ac03b51292bb1e82d123b3b
Malyshev <malyshev.dev@ya.ru>
2017-08-04 15:42:30 +0300

    Initial Commit

---
+++ menu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  menu
+//
+//  Created by Malyshev on 04.08.17.
+//  Copyright © 2017 Malyshev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
