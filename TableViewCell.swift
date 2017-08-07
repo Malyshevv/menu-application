684ddd8fd2cb8e7d2f709b547f175e14e2421b79
Malyshev <malyshev.dev@ya.ru>
2017-08-07 12:47:26 +0300

    first comit

---
+++ TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  menu
+//
+//  Created by Malyshev on 06.08.17.
+//  Copyright © 2017 Malyshev. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
